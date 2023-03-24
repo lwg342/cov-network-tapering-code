4311a48ca130c7e1a251dc3ca0289113b8d50ada
lwg342@gmail.com <lwg342@gmail.com>
2023-03-24 17:35:03 +0000

    Correct some errors. Before writing simulations.

--- example.py
+++ example.py
@@ -10,13 +10,14 @@ p = 500
 n = 100
 lambd = 100
 rho = 0.6
-alpha = 0.1
+alpha = 0.5
 
 # %%
 index = np.arange(0, p, 1)
 distance_matrix = np.abs(np.subtract.outer(index, index))
 
-measurement_error = 5*generate_poisson_discrete_measurement_error(p, lambd=10)
+measurement_error = 1 * \
+    generate_poisson_discrete_measurement_error(p, lambd=lambd)
 
 
 observed_distance_matrix = distance_matrix + measurement_error
@@ -30,7 +31,7 @@ samples = generate_normal_samples(true_cov, n, p)
 # %%
 cov_model = Covariance(samples)
 sample_cov = cov_model.sample_cov()
-tapering_bandwidth = np.floor(n**(1/(2 * alpha + 1))) 
+tapering_bandwidth = np.floor(n**(1/(2 * alpha + 1)))
 # tapering_bandwidth = np.floor(n**(1/2))
 
 taper_cov = cov_tapering(sample_cov, observed_distance_matrix,
@@ -39,13 +40,17 @@ banding_bandwidth = np.floor((n/np.log(p))**(1/(2*alpha + 2)))
 banding_cov = cov_tapering(sample_cov, observed_distance_matrix,
                            bandwidth=banding_bandwidth, method="banding")
 
-print(f"Tapering Bandwidth: {tapering_bandwidth}")
-# %%
-tapering_bandwidth = get_bandwidth(n,p,"tapering", alpha)
-tapering_bandwidth_undersmooth = get_bandwidth(n,p, "tapering_undersmoothing", alpha)
-banding_bandwidth = get_bandwidth(n,p,"banding", alpha)
 
-estimators = {"Sample Covariance": cov_model.sample_cov(),
+# %%
+tapering_bandwidth = get_bandwidth(n, p, "tapering", alpha)
+tapering_bandwidth_undersmooth = get_bandwidth(
+    n, p, "tapering_undersmoothing", alpha)
+banding_bandwidth = get_bandwidth(n, p, "banding", alpha)
+
+print(f"Tapering Bandwidth: {tapering_bandwidth}\nBanding Bandwidth: {banding_bandwidth}\nTaperingBandwidth Undersmooth: {tapering_bandwidth_undersmooth}")
+
+estimators = {
+              "Sample Covariance": cov_model.sample_cov(),
               "Just Diagonal": np.diag(np.diag(sample_cov)),
               "Thresholding": POET(samples.T, K=0, C=0.5).SigmaU,
               "Network Banding": banding_cov,
@@ -57,13 +62,20 @@ estimators = {"Sample Covariance": cov_model.sample_cov(),
               "Banding with True Distance Matrix": cov_tapering(sample_cov, distance_matrix, bandwidth=banding_bandwidth, method="banding"),
               "Tapering with True Distance Matrix": cov_tapering(sample_cov, distance_matrix, bandwidth=tapering_bandwidth, method="linear"),
               "Tapering with True Distance Matrix Undersmoothing": cov_tapering(sample_cov, distance_matrix, bandwidth=tapering_bandwidth_undersmooth, method="linear"),
+              "Network Tapering Undersmoothing Alt": cov_tapering(sample_cov, observed_distance_matrix, bandwidth=np.floor((n/np.log(p))**0.5), method="linear"),
               }
 
 norm_list = ["fro", 2]
-pd.DataFrame([[name, norm, f"{LA.norm(true_cov - estimator, norm): .2f}", test_if_positive_definite(estimator), compute_norm_inverse_difference(true_cov, estimator, norm)]
-              for norm in norm_list for name, estimator in estimators.items()]).set_index([0])
+result = {}
+for norm in norm_list:
+    result[norm] = pd.DataFrame([[name, norm, f"{LA.norm(true_cov - estimator, norm): .2f}", test_if_positive_definite(estimator), compute_norm_inverse_difference(true_cov, estimator, norm)]
+                                 for name, estimator in estimators.items()])
+    result[norm].columns = ["Estimator", "Norm Type",
+                            "Norm", "Positive Definite", "Difference of Inverse"]
+    result[norm] = result[norm].set_index("Estimator")
 
-
+result["fro"]
+result[2]
 # %% PLOTS
 # heatmap(banding_cov, "Banding Covariance")
 heatmap(taper_cov - true_cov, "Tapering Covariance")
@@ -75,7 +87,7 @@ heatmap(observed_distance_matrix, "Observed Distance Matrix")
 plt.hist(measurement_error.reshape(-1))
 # %%
 compute_smallest_eigenvalue(estimators["Network Tapering"])
-# # %% 
+# # %%
 # Test if changing the bandwidth changes the positive definiteness
 
 # for j in range(50):
